aa5cb9008e5293b35b53bc5d81f25e26716b0a06
Naghmeh Shirazi <naghmehd@gmail.com>
2017-03-10 19:11:24 -0600

    added the solution

--- get _list _sum _recursively.rb
+++ get _list _sum _recursively.rb
@@ -1,9 +1,20 @@
+# Get list sum recursively
 # Write function sumR which returns the sum of values in a given list. 
-# try no to cheat and provide recursive solution.
+# Try no to cheat and provide recursive solution.
 
 def sum_r(x)
+#I am assuming x is array
+#this reduces the array by adding every element inthe collection
+  x.reduce(0, :+)
 end
 
+# or using recursive solution: 
+def sum_r(x)
+  return 0 unless x.length > 0
+  return x.shift + sum_r(x)
+end
+
+test: 
 describe "sum_r" do
   it "should add values in a given list" do
     Test.assert_equals(sum_r([]), 0, "Empty case not implemented")
