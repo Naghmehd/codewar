9bd190f6ada828f56d8f9b35d4c161a88b6a2de7
Naghmeh Shirazi <naghmehd@gmail.com>
2017-03-31 22:46:50 -0500

    added codewar solutions to create phone number

--- create_phone_number.rb
+++ create_phone_number.rb
@@ -13,5 +13,15 @@ def createPhoneNumber(numbers)
     "(#{$1}) #{$2}-#{$3}"
 end
 
+# Ideal Solution
+def createPhoneNumber(numbers)
+  "(%d%d%d) %d%d%d-%d%d%d%d" % numbers
+end
+
+#Codewar solution:
+def createPhoneNumber(numbers)
+  numbers.join.gsub /(\d{3})(\d{3})(\d{4})/, '(\1) \2-\3'
+end
+
 Example Tests:
 Test.assert_equals(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
